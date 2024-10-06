ea231f64d34dbcf9a5953153503292978803347b
devesh.mishra <devesh.mishra@intrcity.com>
2024-10-06 22:54:27 +0530

    test case by title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>First React Test case</p>
+      <img title="without copyright image" src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from "./App";
 test("Test First React app case", () => {
   render(<App />);
   const text = screen.getByText(/First React Test Case/i); //testing with case insensitivity
+  const iamgetitle = screen.getByTitle("without copyright image");
   expect(text).toBeInTheDocument();
+  expect(iamgetitle).toBeInTheDocument();
 });
