060bbc5062b8baf99d98c20df8193c95da2dd59a
devesh.mishra <devesh.mishra@intrcity.com>
2024-10-08 00:00:17 +0530

    multiple testing file added

---
+++ src/Second.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("Second Test React app case", () => {
+  render(<App />);
+  const text = screen.getByText(/First React Test Case/i); //testing with case insensitivity
+  const iamgetitle = screen.getByTitle("without copyright image");
+  expect(text).toBeInTheDocument();
+  expect(iamgetitle).toBeInTheDocument();
+});
+
+test("Second Testing input box", () => {
+  render(<App />);
+  const checkInput = screen.getByRole(`textbox`);
+  const checkInputPlaceholer = screen.getByPlaceholderText("Enter User Name");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceholer).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "username");
+  expect(checkInput).toHaveAttribute("id", "username");
+  expect(checkInput).toHaveAttribute("type", "text");
+  expect(checkInput).toHaveAttribute("value", "Devesh Mishra");
+});
