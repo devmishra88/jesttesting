8de6fcc5eb2f0a98fd04e23cd86f92ddac70a476
devesh.mishra <devesh.mishra@intrcity.com>
2024-10-16 00:09:28 +0530

    before and after test hook info

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,7 +1,16 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("on click event testing", () => {
+// beforeAll(()=>{
+//   console.log("********I am before all hook test********")
+// })
+
+// beforeEach(()=>{
+//   console.log("********I am before all hook test********")
+// })
+
+test("on click event testing 1", () => {
+  console.log(`test 1`)
   render(<App />);
   let button = screen.getByRole("button");
 
@@ -9,3 +18,31 @@ test("on click event testing", () => {
 
   expect(screen.getByText("I am updated title")).toBeInTheDocument();
 });
+
+test("on click event testing 2", () => {
+  console.log(`test 2`)
+  render(<App />);
+  let button = screen.getByRole("button");
+
+  fireEvent.click(button)
+
+  expect(screen.getByText("I am updated title")).toBeInTheDocument();
+});
+
+test("on click event testing 3", () => {
+  console.log(`test 3`)
+  render(<App />);
+  let button = screen.getByRole("button");
+
+  fireEvent.click(button)
+
+  expect(screen.getByText("I am updated title")).toBeInTheDocument();
+});
+
+// afterAll(()=>{
+//   console.log("********I am after all hook test********")
+// })
+
+afterEach(()=>{
+  console.log("********I am after each hook test********")
+})
